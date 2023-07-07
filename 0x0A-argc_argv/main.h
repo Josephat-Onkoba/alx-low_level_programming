93631089b84f5fb64a681a8d7be2ce58d5337756
Josephat-Onkoba <josephatonkoba30@gmail.com>
2023-07-07 14:49:07 -0700

    added description for function main

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdio.h>
+#include <stdlib.h>
+
+/* _putchar - Custom function to output a single character */
 int _putchar(char c);
 
 #endif /* MAIN_H */
